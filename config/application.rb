a114f8c8da043b7b36e3faffec9a2b2deb5718f7
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-06-22 17:39:06 -0500

    Intento de internacionalización 1.1.1

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Prueba
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locale', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
     config.i18n.enforce_available_locales = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
