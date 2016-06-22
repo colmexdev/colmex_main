56461724877440ecb8be5018f200934b7179d838
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-06-22 18:22:14 -0500

    Intento de internacionalización 1.1.2

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -1,10 +1,10 @@
 function cambiarClasesIdioma(){
 	this.style.color="#993366";
 	if(this.id=="idioma_en"){
-		document.getElementById("idioma_sp").color="#C0C0C0";
+		document.getElementById("idioma_sp").style.color="#C0C0C0";
 	}
 	else{
-		document.getElementById("idioma_en").color="#C0C0C0";
+		document.getElementById("idioma_en").style.color="#C0C0C0";
 	}
 	return false;
 }
