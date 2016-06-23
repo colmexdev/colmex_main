92f70fe3359219a9ba868837916e801d458d2d37
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-06-23 11:47:51 -0500

    Obteniendo valor de locale para selección de idioma

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -1,11 +1,9 @@
 class PrincipalController < ApplicationController
   def principal
-    $idioma = I18n.locale
   end
 
   def set_language
     I18n.locale = params[:id]
-    $idioma = params[:id]
-    redirect_to :back
+    #redirect_to :back
   end
 end
