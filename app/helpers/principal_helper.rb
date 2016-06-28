7da0f01260b21c840d322bad07fe822b4a73ac7c
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-06-28 17:00:57 -0500

    Conectando con SQL Server para agenda

--- app/helpers/principal_helper.rb
+++ app/helpers/principal_helper.rb
@@ -1,24 +1,24 @@
 module PrincipalHelper
 
   def construir_slider_eventos
-    (slides = (@resultado.each.length / 4).ceil
+    slides = (@resultado.each.length / 4).ceil
     if slides == 0
       slides = 1
     end
     j = 0
-    for i in 1..slides
+    for i in 1..slides 
       k = 1
-      '<div class=' + (i==1 ? '"item active"' : '"item"') + '>'
+      '<div class='.html_safe + (i==1 ? '"item active"'.html_safe : '"item"'.html_safe) + '>'.html_safe
       while k % 4 != 0
-        '<div class="evento">'
+        '<div class="evento">'.html_safe
         if j < @resultado.each.length
-          @resultado.each[j]["tituloActividad"]
+          @resultado.each[j]["tituloActividad"].html_safe
         end
-        '</div>'
+        '</div>'.html_safe
         k = k + 1
         j = j + 1
       end
-      '</div>' 
-    end).html_safe
+      '</div>'.html_safe 
+    end
   end
 end
