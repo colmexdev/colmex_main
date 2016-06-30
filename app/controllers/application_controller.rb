867c5c80e3d4971e68447f38fe536fc6386ab49d
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-06-30 13:03:43 -0500

    Integrando login de administradores

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    redirect_to root_path
+    root_path
   end
 
   def configure_permitted_parameters
