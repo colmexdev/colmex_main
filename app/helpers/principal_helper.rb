81c01a68671e433eb55d164dfcbb5763f2efeeed
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-01 17:36:29 -0500

    Refactorizando código para integrar gestión de imágenes en slider

--- app/helpers/principal_helper.rb
+++ app/helpers/principal_helper.rb
@@ -5,10 +5,10 @@ module PrincipalHelper
     html_block = ""
     control_block = ""
     Slider.all.each do |im|
-      control_block = control_block + '<li data-target="#myCarousel" data-slide-to="' + i.to_s + '"' + (i == 0 ?  'class="active"' : "") + '"></li>'
+      control_block = control_block + '<li data-target="#myCarousel" data-slide-to="' + i.to_s + '"' + (i == 0 ?  'class="active"' : '') + '></li>'
 
 
-      html_block = html_block + '<div class="item ' + (i == 0 ?  "active" : "") + '>'
+      html_block = html_block + '<div class="item ' + (i == 0 ?  "active" : "") + '">'
       html_block = html_block + link_to(image_tag(im.imagen_file_name),im.liga)
       html_block = html_block + '</div>'
       i = i + 1
