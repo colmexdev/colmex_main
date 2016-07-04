55569238eb15432b3762bae0c2882f68e2116d4b
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-04 11:20:28 -0500

    Corrigiendo el hover en menús

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -1,18 +1,9 @@
-function cambiarClasesIdioma(link){
-	link.style.color="#993366";
-	if(link.id=="idioma_en"){
-		document.getElementById("idioma_sp").style.color="#C0C0C0";
-	}
-	else{
-		document.getElementById("idioma_en").style.color="#C0C0C0";
-	}
-	return false;
-}
 
-function borrarClase(){
+
+function borrarClase(element){
   dropdown1 = document.getElementById("dropdown1");
   dropdown2 = document.getElementById("dropdown2");
   dropdown3 = document.getElementById("dropdown3");
-  console.log(this.id);
+  console.log(element.id);
   
 }
