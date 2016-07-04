683bba7d922dd6e266e6eb456c30e214c876b942
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-04 11:41:13 -0500

    Corrigiendo el hover en menús

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -3,16 +3,16 @@ function borrarClase(element){
   dropdown2 = document.getElementById("dropdown2");
   dropdown3 = document.getElementById("dropdown3");
   if(element.id == "dropdown1"){
-    document.getElementById("dropdown2").className = "dropdown";
-    document.getElementById("dropdown3").className = "dropdown";
+    dropdown2.className = "dropdown";
+    dropdown3.className = "dropdown";
   }
   else if(element.id == "dropdown2"){
-    document.getElementById("dropdown1").className = "dropdown";
-    document.getElementById("dropdown3").className = "dropdown";
+    dropdown1.className = "dropdown";
+    dropdown3.className = "dropdown";
   }
   else if(element.id == "dropdown3"){
-    document.getElementById("dropdown2").className = "dropdown";
-    document.getElementById("dropdown1").className = "dropdown";
+    dropdown1.className = "dropdown";
+    dropdown2.className = "dropdown";
   }
   element.className = "dropdown open";
   
