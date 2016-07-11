d09aa5eef261af84fbe0710191d05e3be7c9dd57
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-11 10:33:48 -0500

    Haciendo slider de eventos responsivo

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -1,6 +1,3 @@
-$(window).on("resize",function(event){
-	var ancho = $(window).width();
-	console.log(ancho);
-  //if(ancho < 900){ console.log("Ancho mínimo"); }
-	//else{ console.log("Ancho óptimo"); }
+$(window).resize(function(){
+	console.log($(window).width());
 });
