b433d83a41f2796154676f823e1b62bf95635191
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-11 12:08:44 -0500

    Haciendo slider de eventos responsivo

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -1,7 +1,4 @@
-var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
-window.addEventListener('resize', parseHTML(gon.ev_big, gon.ev_small), true);
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
 
 function parseHTML(divs_big, divs_small){
 	var ancho_act = $(window).width();
