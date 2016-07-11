6f0d4847619192f4e30434e0945c3e21b0c7b4b3
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-11 11:43:47 -0500

    Haciendo slider de eventos responsivo

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -3,19 +3,19 @@ var html1;
 var html2;
 
 $(document).ready(function(){
-	html1 = $('#html_big').innerHTML;
-	html2 = $('#html_small').innerHTML;
-	$(window).resize(parseHTML());
+	console.log("Hola mundo");
+	$(window).resize(parseHTML(gon.ev_big, gon.ev_small));
 });
 
 function parseHTML(divs_big, divs_small){
 	var ancho_act = $(window).width();
-	if(ancho_act < 900 && ancho >= 900){
+	console.log(ancho);
+	/*if(ancho_act < 900 && ancho >= 900){
 		console.log(divs_small);
 		ancho = ancho_act;
 	} 
 	else if(ancho_act >= 900 && ancho < 900){
 		console.log(divs_big);
 		ancho = ancho_act;
-	}
+	}*/
 }
