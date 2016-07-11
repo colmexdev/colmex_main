1ca571972bc67c145b72fa9c5483a113bae04be6
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-11 12:33:44 -0500

    Haciendo slider de eventos responsivo

--- app/assets/javascripts/imagens.js
+++ app/assets/javascripts/imagens.js
@@ -8,17 +8,3 @@ function ocultarCategoria(){
 		document.getElementById("campo_categoria").style.display = "block";
 	}
 }
-
-ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
-
-window.onresize = function(){
-	var ancho_act = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);;
-	if(ancho_act < 900 && ancho >= 900){
-		console.log(gon.ev_small);
-		ancho = ancho_act;
-	} 
-	else if(ancho_act >= 900 && ancho < 900){
-		console.log(gon.ev_big);
-		ancho = ancho_act;
-	}
-}
