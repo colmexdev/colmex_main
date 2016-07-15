a7cae2c4216e06ddc8323ce048a8738f62d58c60
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-15 11:09:11 -0500

    Corrigiendo formulario

--- app/controllers/cursos_controller.rb
+++ app/controllers/cursos_controller.rb
@@ -24,6 +24,9 @@ class CursosController < ApplicationController
   # POST /cursos
   # POST /cursos.json
   def create
+    if curso_params["programa"] != "curso" 
+      curso_params["tipo_curso"] = ""
+    end
     @curso = Curso.new(curso_params)
 
     respond_to do |format|
