87bcbb74ef4cea98bf5dc4735cb6f2b0f37f2a27
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-22 12:18:49 -0500

    Corrigiendo modelo de BD

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,8 +1,9 @@
 class Content < ActiveRecord::Base
 
-    has_attached_file :icono, :styles => {},
+    has_attached_file :icono, :styles => {:small => "20x20"},
                     :url => "/assets/contenidos/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/contenidos/:id/:style/:basename.:extension"
+                    :path => ":rails_root/public/assets/contenidos/:id/:style/:basename.:extension",
+                    :default_url => '/link.jpg';
 
   validates_presence_of :tipo
 
