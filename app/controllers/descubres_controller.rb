1c255d21d7a1685c782e2b744c717133da358036
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-25 11:42:03 -0500

    Agregando tags a Descubre

--- app/controllers/descubres_controller.rb
+++ app/controllers/descubres_controller.rb
@@ -24,6 +24,7 @@ class DescubresController < ApplicationController
   # POST /descubres
   # POST /descubres.json
   def create
+    params[:tags] = params[:tags].to_s.downcase.split(",")
     @descubre = Descubre.new(descubre_params)
     respond_to do |format|
       if @descubre.save
