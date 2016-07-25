660a50f6caedd20b000a0d43706c837011785bd8
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-25 12:20:32 -0500

    Agregando tags a Descubre

--- app/controllers/descubres_controller.rb
+++ app/controllers/descubres_controller.rb
@@ -72,7 +72,7 @@ class DescubresController < ApplicationController
     end
 
     def convertir_array(hash)
-      hash[:descubre][:tags] = hash[:descubre][:tags].to_s.downcase.split(",")
+      hash[:descubre][:tags] = hash[:descubre][:tags].downcase.split(",")
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
