bb7aefd8798437893d12abb65510e04fe51f6348
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-25 13:30:39 -0500

    Agregando tags a Descubre

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -3,8 +3,12 @@ class PrincipalController < ApplicationController
     cliente = TinyTds::Client.new username: 'agendaPRED', password: '@g3NDa#', host: '172.16.40.169', port: '49435'
     @resultado = cliente.execute("USE Agenda")
     @resultado.do
-    @resultado = cliente.execute("SELECT * from dbo.vw_DatosAgenda")
-    gon.ev_big, gon.ev_small, gon.ev_tiny = construye_slider_eventos(@resultado)
+    if not @cliente.dead?
+      @resultado = cliente.execute("SELECT * from dbo.vw_DatosAgenda")
+      gon.ev_big, gon.ev_small, gon.ev_tiny = construye_slider_eventos(@resultado)
+    else
+      gon.ev_big, gon.ev_small, gon.ev_tiny = "", "" ,""
+    end
     @sliders = Slider.where("fecha_expiracion > ? ", Date.current()).order("RAND()")
     @descubre = Descubre.where("fecha_publicacion <= ? AND fecha_limite_pub > ?", Date.current(), Date.current()).order("RAND()")
     @frase = Frase.order("RAND()").first
