441d6d57e9f16f71b01119238c53e895ed154774
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-25 13:34:44 -0500

    Emergencia si la Base de Agenda no está disponible

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -2,7 +2,7 @@ class PrincipalController < ApplicationController
   def principal
     cliente = TinyTds::Client.new username: 'agendaPRED', password: '@g3NDa#', host: '172.16.40.169', port: '49435'
     @resultado = cliente.execute("USE Agenda")
-    if not @cliente.dead?
+    if not cliente.dead?
       @resultado.do
       @resultado = cliente.execute("SELECT * from dbo.vw_DatosAgenda")
       gon.ev_big, gon.ev_small, gon.ev_tiny = construye_slider_eventos(@resultado)
