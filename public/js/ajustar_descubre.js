1e5f55bcd2cade1df71eb2a1d7bd587b7631418f
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-07-26 16:45:10 -0500

    Dando relación 16:9 a slider

--- public/js/ajustar_descubre.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    			var cw = $('.frame-descubre').width();
-					$('.frame-descubre').css({'height':cw+'px'});
-				});
-
-				window.onresize = function(){
-					var cw = $('.frame-descubre').width();
-					$('.frame-descubre').css({'height':cw+'px'});
-				}
