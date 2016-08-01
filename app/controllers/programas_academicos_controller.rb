134f86c7bd3b566b1cb58f1c369050efc7b5da16
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-01 18:44:01 -0500

    Integrando páginas

--- app/controllers/programas_academicos_controller.rb
+++ app/controllers/programas_academicos_controller.rb
@@ -1,8 +1,8 @@
 class ProgramasAcademicosController < ApplicationController
   def inicio
-    @licenciaturas = Curso.where(:programa => "Licenciatura").pluck([:titulo, :traduccion_tit, :liga])
-    @maestrias = Curso.where(:programa => "Maestría").pluck([:titulo, :traduccion_tit, :liga])
-    @doctorados = Curso.where(:programa => "Doctorado").pluck([:titulo, :traduccion_tit, :liga])
+    @licenciaturas = Curso.where(:programa => "Licenciatura").pluck(:titulo, :traduccion_tit, :liga)
+    @maestrias = Curso.where(:programa => "Maestría").pluck(:titulo, :traduccion_tit, :liga)
+    @doctorados = Curso.where(:programa => "Doctorado").pluck(:titulo, :traduccion_tit, :liga)
   end
 
   def admisiones
