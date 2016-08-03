123734112f5dc31963901ae3f8e6999787aea553
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-03 11:14:19 -0500

    Segunda tanda de cambios (hasta Historia)

--- app/helpers/principal_helper.rb
+++ app/helpers/principal_helper.rb
@@ -92,8 +92,8 @@ module PrincipalHelper
     end
   end
 
-  def construir_link(imagen, imagen_hover, texto, liga, clase)
-    return ('<a target="_blank" href="' + raw(liga) + '" class="' + raw(clase) + '">' + ActionController::Base.helpers.image_tag(imagen, :class => "leave_img") + ActionController::Base.helpers.image_tag(imagen_hover, :class => "hover_img") + raw(texto) + "</a>").html_safe
+  def construir_link(imagen, imagen_hover, texto, liga, clase, externa = true)
+    return ('<a ' + (externa ? 'target="_blank"' : '' ) + 'href="' + raw(liga) + '" class="' + raw(clase) + '">' + ActionController::Base.helpers.image_tag(imagen, :class => "leave_img") + ActionController::Base.helpers.image_tag(imagen_hover, :class => "hover_img") + raw(texto) + "</a>").html_safe
   end
 
   def construir_encabezado(imagen, imagen_hover, texto)
