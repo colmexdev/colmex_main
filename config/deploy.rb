9b759c781ae6d50fdc81d7f82182639a5c1bc60c
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-08 13:04:05 -0500

    Probando aproximación de reeajuste de badge

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/RickS1/colmex_main'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/erik/prueba'
+set :deploy_to, '/home/webuser/principal_colmex'
 
 # Default value for :scm is :git
 # set :scm, :git
