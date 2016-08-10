5df47a4fe6ec82dcd842b44bea3112cd2762a51c
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-10 15:35:10 -0500

    Integrando menú

--- public/js/ajustes_tamanio.js
+++ public/js/ajustes_tamanio.js
@@ -37,8 +37,8 @@ $(document).scroll(function() {
   var y = $(this).scrollTop();
   var ancho_act = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
   if (y > 300) {
-	$("#contenedor_nav").css("height", "60px");
-	$("#barra_nav").css({"height":"100%","background-color":"#fff"});
+	$("#contenedor_nav").css({"height":"60px","background-color":"#fff"});
+	$("#barra_nav").css({"height":"100%","background-color":"#fff", "max-width":"1200px"});
 	$("#div_logo").css("display", "none");
 	$("#div_menu").css("display", "none");
 	$(".reajustable").css("display","inline-block");
@@ -58,6 +58,7 @@ $(document).scroll(function() {
 	  $("#header-links").css("display", "none");
 	}
   } else {
+	$("#contenedor_nav").removeAttr("style");
 	$(".reajustable").removeAttr("style");
 	$("#logo-main").removeAttr("style");
 	$("#div_logo").removeAttr("style");
