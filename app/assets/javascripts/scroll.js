80dfee00cd95548c615e3a56eae543f035289c34
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-12 16:26:44 -0500

    Acomodando imagen para tablets

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,7 +1,6 @@
 $(document).on("scroll", function(event) {
   var y = $(this).scrollTop();
   var ancho_act = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
-  console.log("Dentro de scroll: " +barra_render);
   if ((y > 300 && document.body.scrollHeight > window.innerHeight * 2.2 ) || barra_render) {
 	$(".reajustable").removeAttr("style");
 	$("#contenedor_nav").css({"min-height":"60px","background-color":"#fff","-webkit-box-shadow":"0 6px 12px rgba(0,0,0,0.175)","-moz-box-shadow":"0 6px 12px rgba(0,0,0,0.175)","box-shadow":"0 6px 12px rgba(0,0,0,0.175)"});
@@ -11,10 +10,12 @@ $(document).on("scroll", function(event) {
 	$(".reajustable").css("display","inline-block");
 	$("#logo-main").css({"display":"block","padding-left":"50px"});
 	if(ancho_act > 1160){
+	  $("#idioma-big-scroll").css("display","block");
+	  $("#idioma-small-scroll").removeAttr("style");
+	  $(".idioma").css("display","none");
 	  $(".mail").css("display","none");
 	  $(".intranet").css("display","none");	
 	  $(".borrable").css("display","none");
-	  $(".idioma a").css({ "color" : "#333", "line-height":"60px","float":"right"});
 	  $(".navbar-right").css("display","none");
 	  $("#header-links").css({"display":"inline-block", "background-color":"#fff", "padding-left":"50px","float":"right" });
 	  $("#boton-top").css("display","block");
@@ -26,6 +27,9 @@ $(document).on("scroll", function(event) {
 	  else{
 	  	 $(".reajustable").removeAttr("style");
 	  }
+	  $("#idioma-small-scroll").css("display","block");
+	  $("#idioma-big-scroll").removeAttr("style");
+	  $(".idioma").css("display","none");
 	  $("#boton-top").css("display","block");
 	  $("#logo-main").css("padding-left", "10px");
 	  $(".uk-navbar-toggle").css({"line-height":"60px","padding":"0","float":"right", "margin-top":"0"});
@@ -35,14 +39,15 @@ $(document).on("scroll", function(event) {
 	  $(".intranet").removeAttr("style");
 	  $(".mail a").css("line-height","60px");
 	  $(".intranet a").css("line-height","60px");
-	  $(".idioma a").css({ "color" : "#333", "line-height":"60px","float":"right"});
 	  $(".borrable").removeAttr("style");
 	  $(".navbar-right").removeAttr("style");;
 	  $("#header-links").css("display", "none");
 	}
   } else {
         barra_render = false;
-	$(".idioma a").removeAttr("style");
+	$("#idioma-big-scroll").removeAttr("style");
+	$("#idioma-small-scroll").removeAttr("style");
+	$(".idioma").removeAttr("style");
 	$(".mail a").removeAttr("style");
 	$(".intranet a").removeAttr("style");
 	$("#boton-top").removeAttr("style");
