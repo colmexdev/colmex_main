647c5d609811c27983a902be89ce7a7b5d8e1478
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-16 17:55:38 -0500

    Reajustando sección de cursos

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -4,7 +4,7 @@ class PrincipalController < ApplicationController
       cliente = TinyTds::Client.new username: 'agendaPRED', password: '@g3NDa#', host: '172.16.40.214', port: '49767'
       @resultado = cliente.execute("USE Agenda")
       @resultado.do
-      @resultado = cliente.execute("SELECT * from dbo.vw_DatosAgenda WHERE PARSE(fechaFin AS DATE USING 'es-ES') >= GETDATE() ORDER BY PARSE(fechaFin AS DATE USING 'es-ES')")
+      @resultado = cliente.execute("SELECT *,(PARSE(fechaFin AS DATE USING 'es-ES') FROM dbo.vw_DatosAgenda) AS fecha from dbo.vw_DatosAgenda WHERE fecha >= GETDATE() ORDER BY fecha")
 
       gon.ev_big, gon.ev_small, gon.ev_tiny = construye_slider_eventos(@resultado)
     rescue
