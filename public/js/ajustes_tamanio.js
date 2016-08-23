d5096c69febb3e9ee3f3a6df745f131b331e012a
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-23 15:43:05 -0500

    Acomodando secciones

--- public/js/ajustes_tamanio.js
+++ public/js/ajustes_tamanio.js
@@ -8,6 +8,27 @@ num_filas = 2;
 anchos_badges =[];
 barra_render = false;
 
+(function(doc) {
+
+	var addEvent = 'addEventListener',
+	    type = 'gesturestart',
+	    qsa = 'querySelectorAll',
+	    scales = [1, 1],
+	    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+
+	function fix() {
+		meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+		doc.removeEventListener(type, fix, true);
+	}
+
+	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+		fix();
+		scales = [.25, 1.6];
+		doc[addEvent](type, fix, true);
+	}
+
+}(document));
+
 $(document).on('page:load', function(){ UIkit.init(); })
 
 $(document).ready(function(){
