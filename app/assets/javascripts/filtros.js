1c7d1ea1ac5287efe05330587cc1e6404451545b
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-23 15:00:52 -0500

    Acomodando secciones

--- app/assets/javascripts/filtros.js
+++ app/assets/javascripts/filtros.js
@@ -23,7 +23,7 @@ function renderFrase(arreglo){
 	if(!uno_visible){
 		setTimeout(function(){
 			$("#frase-desc").remove();
-			$("#wrapper").append("<div id=\"frase-desc\">Ningún contenido coincide con su búsqueda.</div>");}, 1001);
+			$("#wrapper").append("<div id=\"frase-desc\">Ningún contenido coincide con su búsqueda.</div>");}, 998);
 	}	
 	else{
 		$("#frase-desc").remove();
