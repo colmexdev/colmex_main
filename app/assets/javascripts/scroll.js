8f672f5bd59ced67eea9e0d73ed0c077b08e2716
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-08-23 10:13:21 -0500

    Acomodando navbar

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -5,8 +5,8 @@ $(document).on("scroll", function(event) {
 	$(".reajustable").removeAttr("style");
 	$("#contenedor_nav").css({"min-height":"60px","background-color":"#fff","-webkit-box-shadow":"0 6px 12px rgba(0,0,0,0.175)","-moz-box-shadow":"0 6px 12px rgba(0,0,0,0.175)","box-shadow":"0 6px 12px rgba(0,0,0,0.175)"});
 	$("#barra_nav").css({"height":"100%","background-color":"#fff", "width":"85%","margin": "0 auto"});
-	$("#div_logo").css({"opacity": "0", "height": ($("#div_logo").height() - 20) + "px", "filter": "Alpha(opacity=50)" });
-	$("#div_menu").css({"opacity": "0", "filter": "Alpha(opacity=50)"});
+	$("#div_logo").css({/*"opacity": "0",*/ "height": ($("#div_logo").height() - 20) + "px", "filter": "Alpha(opacity=50)" });
+	$("#div_menu").css({/*"opacity": "0",*/ "filter": "Alpha(opacity=50)"});
 	$(".reajustable").css("display","inline-block");
 	$("#logo-main").css({"display":"block","padding-left":"50px"});
 	if(ancho_act > 1305){
