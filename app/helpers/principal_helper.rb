cb04b3f4d5c62354ea9155521476e474da520eed
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-02-01 17:56:44 -0600

    Integrando líneas de investigación en Directorio Académico

--- app/helpers/principal_helper.rb
+++ app/helpers/principal_helper.rb
@@ -215,8 +215,8 @@ bloque_html = bloque_html + "<a href=\"" + d.liga + "\" " + (d.contenido.mb_char
 						cont = ActiveDirectory::User.find(:first, :mail => academico["correo"]).as_json
 		        academico.store(:libres,arr_libres)
 		        academico.store(:conts, arr_conts.to_a)
-						academico.store(:email, cont["entry"]["myhash"]["mail"])
-            academico.store(:ext, cont["entry"]["myhash"]["telephonenumber"])
+						academico.store(:email, cont["entry"]["myhash"]["mail"][0])
+            academico.store(:ext, cont["entry"]["myhash"]["telephonenumber"][0])
 		        profes << academico
 		        #j = j + 1
 		      rescue
