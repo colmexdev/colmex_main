d2ce72c4a02f81b4655e757420086695b552ef6e
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-02-16 11:56:22 -0600

    Ajustando Directorio Académico

--- public/js/filtro_directorio.js
+++ public/js/filtro_directorio.js
@@ -84,17 +84,15 @@ function filtrarDirectorio(b = '0'){
 	var academicosFiltradosPorLinea = academicosPorLinea.filter(function(d){ 
 		if(lineasAFiltrar.join("").split("").length == 0) return true;
 		//var linea = limpiarPuntuacion(d.toLowerCase());
-		//if(typeof b === 'object'){
-		//	console.log(limpiarPuntuacion(b.innerHTML));
-		//	return linea.indexOf(limpiarPuntuacion(b.innerHTML.toLowerCase())) != -1;
-		//}
-		//else{ 
-		console.log(d);
+		if(typeof b === 'object'){
+			return d.indexOf(limpiarPuntuacion(b.innerHTML.toLowerCase())) != -1;
+		}
+		else{ 
 		for(var j = 0; j < lineasAFiltrar.length; j++){
 			if(d.indexOf(lineasAFiltrar[j]) == -1) return false;
 		}
 		return true;
-	}/*}*/).top(Infinity);
+	}}).top(Infinity);
 	vista_act = 0;
 	$(".inics").css({"color" : "#909090"});
 	if(typeof b !== 'object' && b != '0'){
