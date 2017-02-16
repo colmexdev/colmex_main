4fa3695613abe5be289c4e26362e3cd4e9a93d0b
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-02-16 13:02:12 -0600

    Ajustando Directorio Académico

--- public/js/filtro_directorio.js
+++ public/js/filtro_directorio.js
@@ -89,9 +89,9 @@ function filtrarDirectorio(b = '0'){
 		//}
 		//else{ 
 		for(var j = 0; j < lineasAFiltrar.length; j++){
-			console.log(lineasAFiltrar[j] + " " + d.indexOf(lineasAFiltrar[j]));
 			if(d.indexOf(lineasAFiltrar[j]) == -1) return false;
 		}
+		console.log(lineasAFiltrar[j] + " " + d.indexOf(lineasAFiltrar[j]));
 		return true;
 	}/*}*/).top(Infinity);
 	vista_act = 0;
