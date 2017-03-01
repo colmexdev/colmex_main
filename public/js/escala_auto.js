f6e0269d996f42b8629a34c1e083035c7c1d3354
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-03-01 11:50:33 -0600

    Cambiando Públicos por Presenciales

--- public/js/escala_auto.js
+++ public/js/escala_auto.js
@@ -1,4 +1,13 @@
+ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
+descubres = clone(JSON.parse(gon.descubres));
+agregarVisible(descubres);
+dataset = crossfilter(descubres);
+descubresPorContenido = dataset.dimension(function(d){ return d.contenido; });
+descubresPorTags = dataset.dimension(function(d){ return (d.titulo + " " + d.contenido + " " + d.tags.replace(/ *, */, " ")).replace(/ +/, " ").toLowerCase()});
+num_filas = 2;
+anchos_badges =[];
 barra_render = false;
+
 
 $(document).on("ready page:change page:load",function(){
 	//initAddThis();
