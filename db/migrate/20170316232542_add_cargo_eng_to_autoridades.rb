eccc08775c83716d787e8e41064d2ee4af895140
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-03-16 17:34:11 -0600

    Integrando sección inglés de Historia

--- db/migrate/20170316232542_add_cargo_eng_to_autoridades.rb
+++ db/migrate/20170316232542_add_cargo_eng_to_autoridades.rb
@@ -1,6 +1,6 @@
 class AddCargoEngToAutoridades < ActiveRecord::Migration
   def change
-    add_column :personal, :cargo_eng, :text
-    add_column :personal, :depto_eng, :text
+    add_column :personals, :cargo_eng, :text
+    add_column :personals, :depto_eng, :text
   end
 end
