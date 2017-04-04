ab5f2d50750c7486342d862b6bb06cf1ef055111
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-04-04 13:06:13 -0500

    Aplicando correcciones en inglés

--- app/controllers/programas_academicos_controller.rb
+++ app/controllers/programas_academicos_controller.rb
@@ -68,6 +68,7 @@ class ProgramasAcademicosController < ApplicationController
   end
 
   def catedras_y_seminarios
+		@centros = Catedra.pluck(:tipo)
     @catedras = Catedra.where("tipo = ? AND titulo LIKE ?", "Cátedra", "%México-España%").union(Catedra.where("tipo = ? AND titulo LIKE ?", "Cátedra", "%Humboldt%")).union(Catedra.where("tipo = ? AND titulo LIKE ?", "Cátedra", "%Bourgeois-Pichat%"))
   end
 
