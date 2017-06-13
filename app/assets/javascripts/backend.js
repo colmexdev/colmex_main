ecd363bbe6bd9e39cdc95e42a97a00697fe4ee66
colmexdev <github@colmex.mx>
2017-06-13 17:48:52 -0500

    Estilizando backend

---
+++ app/assets/javascripts/backend.js
@@ -0,0 +1,7 @@
+function changeToggle(over){
+
+		$("#but-toggle").css({"background-color": (over == 0 ? "#DCDCDC" : "")});
+		$("#bar-s").css({"background-color": (over == 0 ? "#A9A9A9" : "")});
+		$("#bar-m").css({"background-color": (over == 0 ? "#A9A9A9" : "")});
+		$("#bar-i").css({"background-color": (over == 0 ? "#A9A9A9" : "")});
+}
