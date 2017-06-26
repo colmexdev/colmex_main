684280b75eb4c9aec158a997d94a0687044737b8
colmexdev <github@colmex.mx>
2017-06-26 17:48:10 -0500

    Estilizando backend (Completitud de funcionalidad)

--- app/models/descubre.rb
+++ app/models/descubre.rb
@@ -3,7 +3,7 @@ class Descubre < ActiveRecord::Base
   has_attached_file :imagen, :styles => {},
                     :url => "/assets/descubre/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/descubre/:id/:style/:basename.:extension",
-                    :default_url => "/vacio.jpg"
+                    :default_url => "/vacio.png"
 
   validates_presence_of :titulo
   validates_presence_of :liga
