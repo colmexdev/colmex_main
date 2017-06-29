0de26fab3c6364bd57be794134675817eb08e7ea
colmexdev <github@colmex.mx>
2017-06-29 18:26:50 -0500

    Estilizando backend (Completitud de funcionalidad)

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -64,11 +64,11 @@ function eje(o,escala,n_ticks,s_ticks,p_ticks,f_ticks){
 }
 
 function completaFechas(f_i,f_f){
-	var fechas = [f_i];
+	var fs = [];
 	var dif_dias = Math.ceil((f_f-f_i)/(24000*3600));
 	for(var i=0; i<dif_dias; i){
-		console.log(new Date(fechas[fechas.length-1].getTime()+(i*24000*3600)));
-		fechas.push({fecha: new Date(fechas[fechas.length-1].getTime()+(i*24000*3600)), val: 0});
+		console.log(new Date(fs[fs.length-1].getTime()+(i*24000*3600)));
+		fs.push({fecha: new Date(f_i+(i*24000*3600)), val: 0});
 	}
-	return fechas
+	return fs
 }
