6f630855f6e272fe26ea350ca95256f2cf0e03fb
colmexdev <github@colmex.mx>
2017-08-25 10:57:40 -0500

    Ajustando Convocatorias

--- config/routes.rb
+++ config/routes.rb
@@ -32,131 +32,84 @@ Rails.application.routes.draw do
   put 'panel/editar' => 'panel#actualizar'
   patch 'panel/editar' => 'panel#actualizar'
 
-  get 'centros_de_estudio/inicio' => "centros_de_estudio#inicio", :as => :centros_de_estudio
+  get 'centros' => "centros_de_estudio#inicio", :as => :centros_de_estudio
+  get 'ceaa' => "centros_de_estudio#ceaa", :as => :ceaa
+  get 'cedua' => "centros_de_estudio#cedua", :as => :cedua
+  get 'ceh' => "centros_de_estudio#ceh", :as => :ceh
+  get 'cei' => "centros_de_estudio#cei", :as => :cei
+  get 'cee' => "centros_de_estudio#cee", :as => :cee
+  get 'cell' => "centros_de_estudio#cell", :as => :cell
+  get 'ces' => "centros_de_estudio#ces", :as => :ces
 
-  get 'centros_de_estudio/ceaa' => "centros_de_estudio#ceaa", :as => :ceaa
+  get 'programas' => "programas_academicos#inicio", :as => :programas_academicos
+  get 'admisiones' => "programas_academicos#admisiones", :as => :admisiones
+  get 'programas-docentes' => 'programas_academicos#programas', :as => :programas
 
-  get 'centros_de_estudio/cedua' => "centros_de_estudio#cedua", :as => :cedua
+  get 'convocatorias' => 'programas_academicos#convocatorias', :as => :convocatorias
+  get 'proceso' => 'programas_academicos#proceso', :as => :proceso
+  get "licenciaturas" => "programas_academicos#licenciaturas", :as => :licenciaturas
+  get "maestrias" => "programas_academicos#maestrias", :as => :maestrias
+  get "doctorados" => "programas_academicos#doctorados", :as => :doctorados
+  get "cursos" => "programas_academicos#cursos", :as => :cursos_varios
+  get 'catedras-y-seminarios' => "programas_academicos#catedras_y_seminarios", :as => :catedras_seminarios
+  get "catedras" => "programas_academicos#catedras", :as => :cats
+  get "seminarios" => "programas_academicos#seminarios", :as => :seminarios
 
-  get 'centros_de_estudio/ceh' => "centros_de_estudio#ceh", :as => :ceh
+  get 'autoridades' => 'informacion_general#autoridades', :as => :autoridades
+  get 'emeritos' => 'informacion_general#profesores_emeritos', :as => :profesores_emeritos
+  get 'historia' => 'informacion_general#historia', :as => :historia
+  get 'sobre' => 'informacion_general#sobre_el_colegio', :as => :sobre_el_colegio
+  get 'transparencia' => 'informacion_general#transparencia', :as => :transparencia
+  get 'bienes' => 'informacion_general#bienes', :as => :bienes_part
+  get 'licitaciones' => 'informacion_general#licitaciones', :as => :licitaciones
+  get 'documentacion-institucional' => 'informacion_general#documentacion_institucional', :as => :documentacion
+  get 'directorio-institucional' => 'informacion_general#directorio_institucional', :as => :dir_inst
+  get 'actas' => 'informacion_general#actas', :as => :actas
+  get 'informes' => 'informacion_general#informes', :as => :informes
+  get 'estatutos' => 'informacion_general#estatutos', :as => :estatutos
+  get 'asamblea' => 'informacion_general#asamblea', :as => :asamblea
+  get 'junta' => 'informacion_general#junta', :as => :junta
+  get 'presidencia' => 'informacion_general#presidencia', :as => :presidencia
+  get 'centros_de_estudio' => 'informacion_general#centros_de_estudio', :as => :centros_autoridades
+  get 'administracion' => 'informacion_general#administracion', :as => :administracion
+  get 'apoyo' => 'informacion_general#unidades_de_apoyo', :as => :unidades_de_apoyo
+  get "comision" => "informacion_general#comision", :as => :comision
+  get "consejo" => "informacion_general#consejo", :as => :consejo
+  get 'info_estudiantes' => 'informacion_general#informacion_para_estudiantes', :as => :info_estudiantes
+  get 'operativas' => 'informacion_general#operativas', :as => :operativas
+  get 'premios-y-distinciones' => 'informacion_general#premios_y_distinciones', :as => :premios
+  get 'historia_75_años' => 'informacion_general#historia_75_años', :as => :historia75
+  get 'comunidad' => 'informacion_general#comunidad', :as => :otorgadoscomunidad
+  get 'colegio' => 'informacion_general#colegio', :as => :otorgadoscolegio
+  get 'directorio-academico' => 'informacion_general#directorio_academico', :as => :directorio
+  get 'marco-normativo' => 'informacion_general#marco_normativo', :as => :marco
+  get 'presenciales' => 'programas_academicos#presenciales', :as => :presenciales
+  get 'electronicos' => 'programas_academicos#electronicos', :as => :electronicos
 
-  get 'centros_de_estudio/cei' => "centros_de_estudio#cei", :as => :cei
+  get 'calendario' => 'programas_academicos#calendario_escolar', :as => :calendario
 
-  get 'centros_de_estudio/cee' => "centros_de_estudio#cee", :as => :cee
+  get 'principios-eticos' => 'principal#principios_eticos', :as => :principios
 
-  get 'centros_de_estudio/cell' => "centros_de_estudio#cell", :as => :cell
+  get '/' => 'principal#principal', :as => :main
 
-  get 'centros_de_estudio/ces' => "centros_de_estudio#ces", :as => :ces
+  get 'resultados_busqueda' => 'principal#resultados_busqueda', :as => :busqueda
 
-  get 'programas_academicos/inicio' => "programas_academicos#inicio", :as => :programas_academicos
+  get 'como-llegar' => 'principal#como_llegar', :as =>  :ubicacion
 
-  get 'programas_academicos/admisiones' => "programas_academicos#admisiones", :as => :admisiones
+  get 'set_language' => 'principal#set_language', :as => :cambiar_idioma
 
-  get 'programas_academicos/programas' => 'programas_academicos#programas', :as => :programas
-
-  get 'programas_academicos/convocatorias' => 'programas_academicos#convocatorias', :as => :convocatorias
-
-  get 'programas_academicos/proceso' => 'programas_academicos#proceso', :as => :proceso
-
-  get "programas_academicos/licenciaturas" => "programas_academicos#licenciaturas", :as => :licenciaturas
-
-  get "programas_academicos/maestrias" => "programas_academicos#maestrias", :as => :maestrias
-
-  get "programas_academicos/doctorados" => "programas_academicos#doctorados", :as => :doctorados
-
-  get "programas_academicos/cursos" => "programas_academicos#cursos", :as => :cursos_varios
-
-  get 'programas_academicos/catedras_y_seminarios' => "programas_academicos#catedras_y_seminarios", :as => :catedras_seminarios
-
-  get "programas_academicos/catedras" => "programas_academicos#catedras", :as => :cats
-
-  get "programas_academicos/seminarios" => "programas_academicos#seminarios", :as => :seminarios
-
-  get 'informacion_general/autoridades' => 'informacion_general#autoridades', :as => :autoridades
-
-  get 'informacion_general/profesores_emeritos' => 'informacion_general#profesores_emeritos', :as => :profesores_emeritos
-
-  get 'informacion_general/historia' => 'informacion_general#historia', :as => :historia
-
-  get 'informacion_general/sobre_el_colegio' => 'informacion_general#sobre_el_colegio', :as => :sobre_el_colegio
-
-  get 'informacion_general/transparencia' => 'informacion_general#transparencia', :as => :transparencia
-
-  get 'informacion_general/bienes' => 'informacion_general#bienes', :as => :bienes_part
-
-  get 'informacion_general/licitaciones' => 'informacion_general#licitaciones', :as => :licitaciones
-
-  get 'informacion_general/documentacion_institucional' => 'informacion_general#documentacion_institucional', :as => :documentacion
-
-  get 'informacion_general/directorio_institucional' => 'informacion_general#directorio_institucional', :as => :dir_inst
-
-  get 'informacion_general/actas' => 'informacion_general#actas', :as => :actas
-
-  get 'informacion_general/informes' => 'informacion_general#informes', :as => :informes
-
-  get 'informacion_general/estatutos' => 'informacion_general#estatutos', :as => :estatutos
-
-  get 'informacion_general/asamblea' => 'informacion_general#asamblea', :as => :asamblea
-
-  get 'informacion_general/junta' => 'informacion_general#junta', :as => :junta
-
-  get 'informacion_general/presidencia' => 'informacion_general#presidencia', :as => :presidencia
-
-  get 'informacion_general/centros_de_estudio' => 'informacion_general#centros_de_estudio', :as => :centros_autoridades
-
-  get 'informacion_general/administracion' => 'informacion_general#administracion', :as => :administracion
-
-  get 'informacion_general/unidades_de_apoyo' => 'informacion_general#unidades_de_apoyo', :as => :unidades_de_apoyo
-
-  get "informacion_general/comision" => "informacion_general#comision", :as => :comision
-
-  get "informacion_general/consejo" => "informacion_general#consejo", :as => :consejo
-
-  get 'informacion_general/informacion_para_estudiantes' => 'informacion_general#informacion_para_estudiantes', :as => :info_estudiantes
-
-  get 'informacion_general/operativas' => 'informacion_general#operativas', :as => :operativas
-
-  get 'informacion_general/premios_y_distinciones' => 'informacion_general#premios_y_distinciones', :as => :premios
-
-  get 'informacion_general/historia_75_años' => 'informacion_general#historia_75_años', :as => :historia75
-
-  get 'informacion_general/comunidad' => 'informacion_general#comunidad', :as => :otorgadoscomunidad
-
-  get 'informacion_general/colegio' => 'informacion_general#colegio', :as => :otorgadoscolegio
-
-  get 'informacion_general/directorio_academico' => 'informacion_general#directorio_academico', :as => :directorio
-
-  get 'informacion_general/marco_normativo' => 'informacion_general#marco_normativo', :as => :marco
-
-  get 'programas_academicos/presenciales' => 'programas_academicos#presenciales', :as => :presenciales
-
-  get 'programas_academicos/electronicos' => 'programas_academicos#electronicos', :as => :electronicos
-
-  get 'programas_academicos/calendario_escolar' => 'programas_academicos#calendario_escolar', :as => :calendario
-
-  get 'principal/principios_eticos' => 'principal#principios_eticos', :as => :principios
-
-  get 'principal/principal'
-
-  get 'principal/resultados_busqueda' => 'principal#resultados_busqueda', :as => :busqueda
-
-  get 'principal/como_llegar' => 'principal#como_llegar', :as =>  :ubicacion
-
-  get 'principal/set_language' => 'principal#set_language', :as => :cambiar_idioma
-
-  get 'informacion_general/directorio_academico_async' => 'informacion_general#directorio_academico_async', :as => :dir_async
-
-  get 'informacion_general/recuperar_docentes' => 'informacion_general#recuperar_docentes', :as => :rec_doc
+  get 'directorio_academico_async' => 'informacion_general#directorio_academico_async', :as => :dir_async
+  get 'recuperar_docentes' => 'informacion_general#recuperar_docentes', :as => :rec_doc
 
   devise_scope :admin do
     get "/acceder" => "devise/sessions#new"
   end
 
-  get "principal/sitemap" => "principal#sitemap", :as => :sitemap
+  get "sitemap" => "principal#sitemap", :as => :sitemap
 
-  get 'principal/proximamente' => 'principal#proximamente', :as => :soon
+  get 'proximamente' => 'principal#proximamente', :as => :soon
 
-  get 'principal/aviso_de_privacidad' => 'principal#aviso_de_privacidad', :as => :aviso
+  get 'aviso-de-privacidad' => 'principal#aviso_de_privacidad', :as => :aviso
 
 
   # The priority is based upon order of creation: first created -> highest priority.
