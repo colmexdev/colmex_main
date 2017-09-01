d6f136672947dfc129774b244b48f553b93f04b3
colmexdev <github@colmex.mx>
2017-09-01 11:40:39 -0500

    Modificaciones al index de backend

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -29,7 +29,7 @@ function buildQuery(conds){
 	console.log(conds);
 	var query = "";
 	for(var i in conds){
-		var filt = $("#query_"+conds[i][0])[0];
+		var filter = $("#query_"+conds[i][0])[0];
 		if(filter.value == "" || filter.value == null) continue
 		else query += (query.length > 0 ? " and " : "") + conds[i][0] + " " + conds[i][1] + " " + (conds[i][2] == 0 ? "'" : ( conds[i][2] == 1 ? "'%'" : "")) + filter.value + (conds[i][2] == 0 ? "'" : ( conds[i][2] == 1 ? "'%'" : ""));
 	}
