960291cafee94e36b3e31a028ed7dfa06eccb130
colmexdev <github@colmex.mx>
2017-09-28 11:48:30 -0500

    Prueba Trix

--- public/js/ajustes_tamanio.js
+++ public/js/ajustes_tamanio.js
@@ -20,18 +20,30 @@ barra_render = false;
 
 $(document).on('page:load', function(){ 
 	UIkit.init(); 
-	$(document).trigger("scroll");  
+	$(document).trigger("scroll");
+	try{
+		console.log("Hola");
+		buttonHTML = "<button type=\"button\" class=\"heading\" data-attribute=\"heading\" title=\"Heading\">Heading</button>";
+		groupElement = Trix.config.toolbar.content.querySelector(".block_tools");
+		groupElement.insertAdjacentHTML("beforeend", buttonHTML);
+		//Trix.config.blockAttributes.heading = { tagName: 'color' };
+	}
+	catch(err){
+		console.log("Error");
+	}  
 })
 
 $(document).on("ready page:change", function(){
 	UIkit.init();
 	try{
+		console.log("Hola");
 		buttonHTML = "<button type=\"button\" class=\"heading\" data-attribute=\"heading\" title=\"Heading\">Heading</button>";
 		groupElement = Trix.config.toolbar.content.querySelector(".block_tools");
 		groupElement.insertAdjacentHTML("beforeend", buttonHTML);
-		Trix.config.blockAttributes.heading = { tagName: 'color' };
+		//Trix.config.blockAttributes.heading = { tagName: 'color' };
 	}
 	catch(err){
+		console.log("Error");
 	}
 	var cw = $('.frame-descubre').width() - 8;
 	try{
