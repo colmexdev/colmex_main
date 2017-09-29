e2ab55d6604af8c51fcb75cace7935b75c36f536
colmexdev <github@colmex.mx>
2017-09-29 15:54:02 -0500

    Añadiendo Contenido de Sitios al panel

--- app/models/foto.rb
+++ app/models/foto.rb
@@ -4,7 +4,7 @@ class Foto < ActiveRecord::Base
                     :path => ":rails_root/public/assets/fotos/:id/:style/:basename.:extension",
                     :default_url => "/vacio.png"
 
-  validates_presence_of :ref
+  validates_presence_of :sitio_index
   validates_presence_of :index
   validates_attachment_content_type :foto, :content_type => ['image/jpeg', 'image/png', 'image/jpg']
 end
--- app/models/parrafo.rb
+++ app/models/parrafo.rb
@@ -1,5 +1,5 @@
 class Parrafo < ActiveRecord::Base
-  validates_presence_of :ref
+  validates_presence_of :sitio_id
   validates_presence_of :texto
   validates_presence_of :index
 end
