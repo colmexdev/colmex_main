e36a400e2d7ec8cc5a996acbfed624e7cde939f4
colmexdev <github@colmex.mx>
2017-10-03 13:39:38 -0500

    Prueba con Sobre el Colegio

--- public/js/ajustes_tamanio.js
+++ public/js/ajustes_tamanio.js
@@ -32,6 +32,7 @@ $(document).on("ready", function(){
 			var blockElement = event.target.toolbarElement.querySelector("[data-trix-button-group='block-tools']");
 			extendTrix(groupElement,blockElement);
 			clearPars(event.srcElement);
+			console.log(event.srcElement);
 			event.srcElement.addEventListener("focus",function(){clearPars(this)});
 		})
 	}
