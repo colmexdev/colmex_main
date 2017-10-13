f5af400a1929823e03e38434883b0d6cd3330f94
colmexdev <github@colmex.mx>
2017-10-13 14:34:12 -0500

    Formulario editable de Historia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :store_admin_location!, if: :storable_location?
   include PrincipalHelper
   include SobreElColegioHelper
 
@@ -34,21 +33,12 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-  private
-    def storable_location?
-      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
-    end
-
-    def store_admin_location!
-      store_location_for(:admin, request.fullpath)
-    end
-
-  def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || super
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || panel_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || super
+    new_admin_session_path
   end
 
   def configure_permitted_parameters
