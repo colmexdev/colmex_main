9200ce9344ad177421f2eec15ac87b729cdd8748
colmexdev <github@colmex.mx>
2018-04-13 13:53:57 -0500

    Nueva categoría

--- db/migrate/20180413184309_add_plurales_to_descubre.rb
+++ db/migrate/20180413184309_add_plurales_to_descubre.rb
@@ -1,4 +1,4 @@
-class AddPluralesToContent < ActiveRecord::Migration
+class AddPluralesToDescubre < ActiveRecord::Migration
   def change
     add_column :contents, :plural, :text
     add_column :contents, :plural_eng, :text
