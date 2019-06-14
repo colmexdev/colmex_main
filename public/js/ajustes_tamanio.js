fdebd3929a27129ebe1ed3052006ca152030500e
colmexdev <github@colmex.mx>
2019-06-14 17:01:52 -0500

    Banner ESALC

--- public/js/ajustes_tamanio.js
+++ public/js/ajustes_tamanio.js
@@ -81,7 +81,7 @@ $(document).on("ready", function(){
 
   agregarAnaliticos();
 	try{
-		escalarBanner("div-banner-pres",230,900);
+		//escalarBanner("div-banner-pres",230,900);
 		setTimeout(function(){
 			var anchos_badges = recabarAnchos();
 			escalarSlider(anchos_badges);
