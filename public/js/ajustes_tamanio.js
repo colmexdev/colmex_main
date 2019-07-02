cc334041aa1d8daa927406afa2f6eb2f78a2f274
colmexdev <github@colmex.mx>
2019-07-02 13:01:46 -0500

    Sliders

--- public/js/ajustes_tamanio.js
+++ public/js/ajustes_tamanio.js
@@ -147,7 +147,7 @@ function escalarSlider(arr_a){
 	var ancho_slide = $("#div_slider").width();
 	var alto_slide = ancho_slide*(17/40);
 	$('#div_slider').css({'height':alto_slide+'px'});
-	$('.ghost-slider').css("height",alto_slide+"px");
+	/*$('.ghost-slider').css("height",alto_slide+"px");*/
 	reajustarBadges(arr_a);
 }
 
@@ -159,7 +159,7 @@ function escalarBanner(el,mh,mw){
 function reajustarBadges(arr){
 	for( var i = 0; i < gon.cant_sliders; i++){
 		var ancho_nuevo = ($("#div_slider").width() * arr[i]) / 1341;
-		$( "#badge-" + i ).css({"width": ancho_nuevo + "px", "max-width": arr[i]+"px" });
+		/*$( "#badge-" + i ).css({"width": ancho_nuevo + "px", "max-width": arr[i]+"px" });*/
 	}
 }
 
